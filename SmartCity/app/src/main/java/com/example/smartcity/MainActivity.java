f6f35f73f6563bfab49f05a293a6dee198a6ddd1
Stefanovski <stefanovski_stefan@hotmail.com>
2020-04-06 16:09:05 +0200

    intarface inscription et  update login

--- SmartCity/app/src/main/java/com/example/smartcity/MainActivity.java
+++ SmartCity/app/src/main/java/com/example/smartcity/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.smartcity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText utilisateurEditText = (EditText)findViewById(R.id.utilisateurEditText);
+        final EditText mdpEditText = (EditText)findViewById(R.id.motdepasseEditText);
+        TextView inscription = (TextView)findViewById(R.id.inscriptionTextView);
+        inscription.setText(Html.fromHtml("<p><u>" + inscription.getText() + "</u></p>"));
+
+        Button connexion = (Button)findViewById(R.id.connexion);
+
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inscripptionIntent = new Intent(MainActivity.this,InscriptionActivity.class);
+                startActivity(inscripptionIntent);
+            }
+        });
+
+        connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String utilisateur = utilisateurEditText.getText().toString();
+                String mdp = mdpEditText.getText().toString();
+                /* database check user TO DO*/
+
+                Intent AcceuilIntent = new Intent(MainActivity.this,AccueilActivity.class);
+                startActivity(AcceuilIntent);
+            }
+        });
     }
 }
