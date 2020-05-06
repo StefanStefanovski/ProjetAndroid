7140bc2bb24636d59aa869ae535ef48819399ad2
stefanovski <stefanovski_stefan@hotmail.com>
2020-05-06 23:30:08 +0200

    update

--- SmartCity/app/src/main/java/com/example/smartcity/ConfigurationActivity.java
+++ SmartCity/app/src/main/java/com/example/smartcity/ConfigurationActivity.java
@@ -1,7 +1,10 @@
 package com.example.smartcity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,40 @@ public class ConfigurationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        Button demandesButton = (Button)findViewById(R.id.DemandesBtn);
+        Button creerReseauButton = (Button)findViewById(R.id.CreerReseauBtn);
+        Button creerEvennementButton  = (Button)findViewById(R.id.CreerEvenementBtn);
+
+        demandesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                Intent DemandesIntent = new Intent(ConfigurationActivity.this, DemandesActivity.class);
+                startActivity(DemandesIntent);
+                */
+            }
+        });
+
+        creerReseauButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                Intent CreerReseauIntent = new Intent(ConfigurationActivity.this, CreerReseauActivity.class);
+                startActivity(CreerReseauIntent);
+            */
+            }
+        });
+
+        creerEvennementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                Intent CreerEvennementIntent = new Intent(ConfigurationActivity.this, CreerEvennementActivity.class);
+                startActivity(CreerEvennementIntent);
+                 */
+            }
+        });
+
     }
 }
