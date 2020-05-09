61cfc1e8beb77772874e1d82b91ffce83284eccc
stefanovski <stefanovski_stefan@hotmail.com>
2020-05-09 15:57:49 +0200

    update

--- SmartCity/app/src/main/java/com/example/smartcity/CommerceActivity.java
+++ SmartCity/app/src/main/java/com/example/smartcity/CommerceActivity.java
@@ -1,14 +1,24 @@
 package com.example.smartcity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class CommerceActivity extends AppCompatActivity {
+public class CommerceActivity extends FragmentActivity {
 
+    FragmentTransaction fragmentTransaction;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commerce);
     }
+
+   
+
 }
