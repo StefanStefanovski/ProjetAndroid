1846df41ae4b0f7f86e08fad4c165d7bc79eaae8
stefanovski <stefanovski_stefan@hotmail.com>
2020-05-09 18:05:59 +0200

    mode annuaire et proximite ajouté

--- SmartCity/app/src/main/java/com/example/smartcity/CommerceActivity.java
+++ SmartCity/app/src/main/java/com/example/smartcity/CommerceActivity.java
@@ -2,6 +2,10 @@ package com.example.smartcity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -9,7 +13,7 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class CommerceActivity extends FragmentActivity {
+public class CommerceActivity extends FragmentActivity implements PopupMenu.OnMenuItemClickListener {
 
     FragmentTransaction fragmentTransaction;
     FragmentManager fragmentManager;
@@ -19,6 +23,24 @@ public class CommerceActivity extends FragmentActivity {
         setContentView(R.layout.activity_commerce);
     }
 
-   
+   public void showPopup (View v){
+       PopupMenu popup = new PopupMenu(this,v);
+       popup.setOnMenuItemClickListener(this);
+       popup.inflate(R.menu.popup_menu);
+       popup.show();
+    }
 
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.Proximite:
+                Toast.makeText(this, "Proximite", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Annuaire:
+                Toast.makeText(this, "Annuaire", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
 }
